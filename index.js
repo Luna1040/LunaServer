f3ace7d8330faefcf609bf3bd38569b3db76c167
liang@rabbitoise.com <Luna1040>
2020-11-16 22:09:53 +0800

    merge

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const client = new MongoClient(uri, {useNewUrlParser: true});
 app.use(exp.static(__dirname + "/public"));
 app.use(parse.json());
 
+// 已验证大多数情况
 app.post("/api/user/getUserInfo", function (req, res) {
     if (req.body.userId) {
         client.connect(err => {
@@ -57,56 +58,6 @@ app.post("/api/user/getUserInfo", function (req, res) {
                 });
             client.close();
         });
-        // MongoClient.connect(
-        //     dburl, {
-        //       useNewUrlParser: true,
-        //     },
-        //     function (err, client) {
-        //       if (err) {
-        //         res.send({
-        //           status: 500,
-        //           success: false,
-        //           code: 0,
-        //           //未知错误
-        //         });
-        //         return;
-        //       }
-        //       const userInfo = client.db("userInfo");
-        //       userInfo
-        //           .collection("users")
-        //           .find({
-        //             uid: req.body.userId,
-        //           })
-        //           .toArray(function (err, resData) {
-        //             console.log(resData);
-        //             if (err) {
-        //               console.log(err);
-        //               res.send({
-        //                 status: 500,
-        //                 success: false,
-        //                 code: 0,
-        //                 //未知错误
-        //               });
-        //               return;
-        //             }
-        //             if (resData.length === 0) {
-        //               res.send({
-        //                 status: 500,
-        //                 success: false,
-        //                 code: 2,
-        //                 //无法获取用户信息，请重新登录！
-        //               });
-        //             }
-        //             delete resData[0].password;
-        //             res.send({
-        //               status: 200,
-        //               success: true,
-        //               data: resData[0],
-        //             });
-        //           });
-        //       client.close();
-        //     }
-        // );
     } else {
         res.send({
             status: 500,
@@ -117,6 +68,7 @@ app.post("/api/user/getUserInfo", function (req, res) {
         return;
     }
 });
+// 已验证大多数情况
 app.post("/api/user/register", function (req, res) {
     let data = req.body;
     if (data.userName === "" || !data.userName) {
@@ -202,64 +154,8 @@ app.post("/api/user/register", function (req, res) {
             });
         })
     })
-    // MongoClient.connect(
-    //   dburl, {
-    //     useNewUrlParser: true,
-    //   },
-    //   function (err, client) {
-    //     if (err) {
-    //       console.log(err);
-    //       res.send({
-    //         status: 500,
-    //         success: false,
-    //         code: 0,
-    //         //未知错误
-    //       });
-    //       return;
-    //     }
-    //     const userInfo = client.db("userInfo");
-    //     userInfo
-    //       .collection("users")
-    //       .find({
-    //         userName: req.body.userName,
-    //       })
-    //       .toArray(function (err, resData) {
-    //         if (err) {
-    //           console.log(err);
-    //           res.send({
-    //             status: 500,
-    //             success: false,
-    //             code: 0,
-    //             //未知错误
-    //           });
-    //           return;
-    //         }
-    //         if (resData.length !== 0) {
-    //           res.send({
-    //             status: 500,
-    //             success: false,
-    //             code: 5,
-    //             // 用户名已存在
-    //           });
-    //           return;
-    //         }
-    //       });
-    //     userInfo.collection("users").insertOne(data, function (err, resData) {
-    //       if (err) {
-    //         return;
-    //       }
-    //       delete resData.ops[0].password;
-    //       res.send({
-    //         status: 200,
-    //         success: true,
-    //         data: resData.ops[0],
-    //       });
-    //       console.log(resData);
-    //     });
-    //     client.close();
-    //   }
-    // );
 });
+// 已验证大多数情况
 app.post("/api/user/login", function (req, res) {
     let data = req.body;
     if (data.userName === "" || !data.userName) {
@@ -360,85 +256,8 @@ app.post("/api/user/login", function (req, res) {
                 });
         }
     })
-    // MongoClient.connect(
-    //   dburl, {
-    //     useNewUrlParser: true,
-    //   },
-    //   function (err, client) {
-    //     if (err) {
-    //       console.log(err);
-    //       return;
-    //     }
-    //     console.log("连接成功！");
-    //     const userInfo = client.db("userInfo");
-    //     if (data.userName.indexOf("@") !== -1) {
-    //       userInfo
-    //         .collection("users")
-    //         .find({
-    //           email: data.userName,
-    //         })
-    //         .toArray(function (err, resData) {
-    //           console.log(resData);
-    //           if (resData.length === 0) {
-    //             res.send({
-    //               status: 500,
-    //               success: false,
-    //               code: 2,
-    //               // 用户名或邮箱错误
-    //             });
-    //             return;
-    //           }
-    //           if (resData[0].password !== data.password) {
-    //             res.send({
-    //               status: 500,
-    //               success: false,
-    //               code: 4,
-    //               // 密码错误！
-    //             });
-    //             return;
-    //           }
-    //           delete resData[0].password;
-    //           res.send({
-    //             status: 200,
-    //             success: true,
-    //             data: resData,
-    //           });
-    //         });
-    //     } else {
-    //       userInfo
-    //         .collection("users")
-    //         .find({
-    //           userName: data.userName,
-    //         })
-    //         .toArray(function (err, resData) {
-    //           if (resData.length === 0) {
-    //             res.send({
-    //               status: 500,
-    //               success: false,
-    //               code: 2,
-    //               // 用户名或邮箱错误
-    //             });
-    //             return;
-    //           }
-    //           if (resData[0].password !== data.password) {
-    //             res.send({
-    //               status: 500,
-    //               success: false,
-    //               code: 4,
-    //               // 密码错误！
-    //             });
-    //             return;
-    //           }
-    //           res.send({
-    //             status: 200,
-    //             success: true,
-    //             data: resData,
-    //           });
-    //         });
-    //     }
-    //   }
-    // );
 });
+// 正在测试
 app.post('/api/home/addProject', function (req, res) {
     let data = req.body;
     if (!data.uid) {
@@ -514,51 +333,6 @@ app.post('/api/home/addProject', function (req, res) {
             })
         })
     })
-    //   MongoClient.connect(
-    //   dburl, {
-    //     useNewUrlParser: true,
-    //   },
-    //   function (err, client) {
-    //     if (err) {
-    //       res.send({
-    //         status: 500,
-    //         success: false,
-    //         code: 0,
-    //         //未知错误
-    //       });
-    //       return;
-    //     }
-    //     const todoList = client.db("todoList");
-    //     todoList.collection(data.uid).find().toArray(function (err, resData) {
-    //       for (let i = 0; i < resData.length; i++) {
-    //         if(resData[i].id === data.id) {
-    //           res.send({
-    //             status: 500,
-    //             success: false,
-    //             code: 3,
-    //             //ID重复
-    //           });
-    //           return
-    //         }
-    //       }
-    //     })
-    //     todoList.collection(data.uid).insertOne(data, function (err, resData) {
-    //       if (err) {
-    //         res.send({
-    //           status: 500,
-    //           success: false,
-    //           code: 0,
-    //           //未知错误
-    //         });
-    //         return;
-    //       }
-    //       res.send({
-    //         status: 200,
-    //         success: true
-    //       })
-    //     })
-    //   }
-    // );
 });
 app.post('/api/home/getProjectList', function (req, res) {
     let data = req.body;
@@ -616,51 +390,6 @@ app.post('/api/home/getProjectList', function (req, res) {
             })
         })
     })
-    //   MongoClient.connect(
-    //   dburl, {
-    //     useNewUrlParser: true,
-    //   },
-    //   function (err, client) {
-    //     if (err) {
-    //       res.send({
-    //         status: 500,
-    //         success: false,
-    //         code: 0,
-    //         //未知错误
-    //       });
-    //       return;
-    //     }
-    //     const todoList = client.db("todoList");
-    //     todoList.collection(data.uid).find().toArray(function (err, resData) {
-    //       for (let i = 0; i < resData.length; i++) {
-    //         if(resData[i].id === data.id) {
-    //           res.send({
-    //             status: 500,
-    //             success: false,
-    //             code: 3,
-    //             //ID重复
-    //           });
-    //           return
-    //         }
-    //       }
-    //     })
-    //     todoList.collection(data.uid).insertOne(data, function (err, resData) {
-    //       if (err) {
-    //         res.send({
-    //           status: 500,
-    //           success: false,
-    //           code: 0,
-    //           //未知错误
-    //         });
-    //         return;
-    //       }
-    //       res.send({
-    //         status: 200,
-    //         success: true
-    //       })
-    //     })
-    //   }
-    // );
 });
 app.post('/api/home/addTodoList', function (req, res) {
     let data = req.body;
@@ -722,51 +451,6 @@ app.post('/api/home/addTodoList', function (req, res) {
             })
         })
     })
-    //   MongoClient.connect(
-    //   dburl, {
-    //     useNewUrlParser: true,
-    //   },
-    //   function (err, client) {
-    //     if (err) {
-    //       res.send({
-    //         status: 500,
-    //         success: false,
-    //         code: 0,
-    //         //未知错误
-    //       });
-    //       return;
-    //     }
-    //     const todoList = client.db("todoList");
-    //     todoList.collection(data.uid).find().toArray(function (err, resData) {
-    //       for (let i = 0; i < resData.length; i++) {
-    //         if(resData[i].id === data.id) {
-    //           res.send({
-    //             status: 500,
-    //             success: false,
-    //             code: 3,
-    //             //ID重复
-    //           });
-    //           return
-    //         }
-    //       }
-    //     })
-    //     todoList.collection(data.uid).insertOne(data, function (err, resData) {
-    //       if (err) {
-    //         res.send({
-    //           status: 500,
-    //           success: false,
-    //           code: 0,
-    //           //未知错误
-    //         });
-    //         return;
-    //       }
-    //       res.send({
-    //         status: 200,
-    //         success: true
-    //       })
-    //     })
-    //   }
-    // );
 });
 app.post('/api/home/getTodoList', function (req, res) {
     let data = req.body;
@@ -811,43 +495,6 @@ app.post('/api/home/getTodoList', function (req, res) {
             })
         })
     })
-    //   MongoClient.connect(
-    //   dburl, {
-    //     useNewUrlParser: true,
-    //   },
-    //   function (err, client) {
-    //     if (err) {
-    //       res.send({
-    //         status: 500,
-    //         success: false,
-    //         code: 0,
-    //         //未知错误
-    //       });
-    //       return;
-    //     }
-    //     let uid = data.uid
-    //     const todoList = client.db("todoList");
-    //     todoList.collection(uid).find().toArray(function (err, resData) {
-    //       if (err) {
-    //         res.send({
-    //           status: 500,
-    //           success: false,
-    //           code: 0,
-    //           //未知错误
-    //         });
-    //         return;
-    //       }
-    //       resData.forEach((item, index) => {
-    //         delete resData[index]._id
-    //       })
-    //       res.send({
-    //         status: 200,
-    //         success: true,
-    //         data: resData
-    //       })
-    //     })
-    //   }
-    // );
 });
 app.listen(3000, function (err) {
     if (err) {
